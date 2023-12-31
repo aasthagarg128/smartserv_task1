12fa6b095d0609f153b5ce246276a9a6e0a6a0ae
aasthagarg128 <aasthagarg128@gmail.com>
2023-12-31 22:40:15 +0530

    work added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+ const fetchData = async () => {
+  try {
+    const response = await axios.get('https://s3.amazonaws.com/open-to-cors/assignment.json');
+    const data = response.data;
+
+    console.log('Fetched data:', data);
+
+    if (!Array.isArray(data)) {
+      throw new Error('Invalid data format. Expected an array.');
+    }
+
+    const sortedProducts = data.sort((a, b) => b.Popularity - a.Popularity);
+    setProducts(sortedProducts);
+  } catch (error) {
+    console.error('Failed to fetch data:', error);
+    setError(`Failed to fetch data. Error: ${error.message}`);
+  }
+};
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Product List</h1>
+      {error ? (
+        <p>{error}</p>
+      ) : (
+        products.map((product, index) => (
+          <div key={index}>
+            <h3>{product.Title}</h3>
+            <p>Price: {product.Price}</p>
+            <p>Popularity: {product.Popularity}</p>
+            <hr />
+          </div>
+        ))
+      )}
     </div>
   );
 }
